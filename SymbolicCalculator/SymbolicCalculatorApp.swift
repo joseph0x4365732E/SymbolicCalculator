94bb728ea4a5abbee03b9144c53640d1629b5b14
Joseph Cestone <josephcestone@icloud.com>
2022-09-01 14:17:13 -0700

    Initial Commit

---
+++ SymbolicCalculator/SymbolicCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolicCalculatorApp.swift
+//  SymbolicCalculator
+//
+//  Created by Joseph Cestone on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SymbolicCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
